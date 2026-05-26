ff350021b9c9f8bd7e97c2fb2ff1cf1cad4a0f7b
Huynh Anh Quy <anhquy19042005@gmail.com>
2026-05-27 00:18:17 +0700

    feat: config persistence.xml and pom.xml to connect to MySQL

--- haq1904_JPAsdemo/src/main/java/com/fe/main/StudentMain.java
+++ haq1904_JPAsdemo/src/main/java/com/fe/main/StudentMain.java
@@ -1,43 +1,73 @@
 package com.fe.main;
 import com.fe.pojo.Student;
 import com.fe.dao.StudentDAO;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class StudentMain {
     public static void main(String[] args){
-        System.out.println("+++++++++MENU+++++++++");
-        System.out.println("+ 1. Get Students +");
-        System.out.println("+ 2. Add Student +");
-        System.out.println("+ 3. Delete Student +");
-        System.out.println("+ 4. Update Student +");
-        System.out.println("+ 5. Delete Student +");
-        System.out.println("+ 6. Get a Student +");
-        System.out.println("+ 0. QUIT +");
-        System.out.println("+++++++++END++++++++++");
+        Scanner console = new Scanner(System.in);
+        StudentDAO studentDAO = new StudentDAO("JPAs");
+
         int inputKey = -1;
         while (inputKey != 0) {
-            Scanner console = new Scanner(System.in);
-            System.out.println("Please enter a number !");
+            // 2. MANG MENU VÀO TRONG VÒNG LẶP ĐỂ IN LẠI MỖI KHI CHẠY XONG 1 CHỨC NĂNG
+            System.out.println("\n+++++++++ MENU +++++++++");
+            System.out.println("+ 1. Get All Students  +");
+            System.out.println("+ 2. Add Student       +");
+            System.out.println("+ 3. Update Student    +");
+            System.out.println("+ 4. Delete Student    +");
+            System.out.println("+ 5. Get a Student     +");
+            System.out.println("+ 0. QUIT              +");
+            System.out.println("+++++++++ END ++++++++++");
+            System.out.print("Please enter a number: ");
+
             inputKey = console.nextInt();
-            StudentDAO studentDAO = new StudentDAO("JPAs");
-            Student student = new Student("Lam", "Nguyen", 9);
+
+            // 3. KHỚP SWITCH-CASE ĐÚNG VỚI MENU PHÍA TRÊN
             switch (inputKey) {
-                case 0:
-                    break;
                 case 1:
-                    studentDAO.save(student);
+                    System.out.println("--- Danh sách Sinh viên ---");
+                    List<Student> students = studentDAO.getStudents();
+                    for ( Student s : students){
+                        System.out.println(s.getId()+ s.getFirstName()+ s.getLastName() + s.getMarks());
+                    }
                     break;
+
                 case 2:
-                    studentDAO.delete(1);
+                    System.out.println("--- Thêm Sinh viên mới ---");
+                    Student newStudent = new Student("Lam", "Nguyen", 9);
+                    studentDAO.save(newStudent);
+                    System.out.println("Đã thêm thành công!");
                     break;
+
                 case 3:
-                    student = new Student("Sang", "Nguyen", 9);
-                    studentDAO.update (student);
+                    System.out.println("--- Cập nhật Sinh viên ---");
+                    Student updateStudent = new Student("Sang", "Nguyen", 9);
+                    // Lưu ý: Thường khi update ông phải set id cho updateStudent để JPA biết sửa dòng nào
+                    studentDAO.update(updateStudent);
+                    System.out.println("Đã cập nhật thành công!");
+                    break; // CỦA ÔNG ĐANG BỊ THIẾU LỆNH BREAK Ở ĐÂY NÈ!
+
                 case 4:
+                    System.out.println("--- Xóa Sinh viên ---");
+                    studentDAO.delete(1);
+                    System.out.println("Đã xóa thành công!");
+                    break;
+
+                case 5:
+                    System.out.println("--- Tìm Sinh viên theo ID ---");
                     studentDAO.findById(1);
                     break;
+
+                case 0:
+                    System.out.println("Thoát chương trình. Tạm biệt ông nha!");
+                    break;
+
                 default:
-                    System.out.println("Please choice menu !");
+                    System.out.println("Vui lòng chọn đúng số trên menu!");
+                    break;
             }
         }
     }
