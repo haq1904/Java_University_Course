441431f0bd42fdb44f5e5cdb28b8e74888773751
Huynh Anh Quy <anhquy19042005@gmail.com>
2026-06-07 15:48:39 +0700

    feat: creat ManyToMany Hibernate demo

---
+++ Hinbernatedemo-ManyToMany/src/main/java/com/fe/pojo/Book.java
@@ -0,0 +1,60 @@
+package com.fe.pojo;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "BOOKS")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title", length = 30)
+    private String title;
+
+    String author;
+
+    String isbn;
+
+    @ManyToMany(mappedBy = "books")
+    private Set<Student> students = new HashSet<Student>();
+
+    public Book() {
+        super();
+    }
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+}
